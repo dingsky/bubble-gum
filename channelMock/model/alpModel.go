d680a2ff60cfb2eb1f8b9e75308434e5a4250af1
mating0815 <sunny.ma@cardinfolink.com>
2018-02-23 15:33:10 +0800

    test

--- channelMock/model/alpModel.go
+++ channelMock/model/alpModel.go
@@ -10,7 +10,7 @@ type AlpCommonResponse struct {
 	Msg         string `json:"msg" bson:"msg"`                               // 结果码描述
 	SubCode     string `json:"sub_code,omitempty" bson:"sub_code,omitempty"` // 错误子代码
 	SubMsg      string `json:"sub_msg,omitempty"  bson:"sub_msg,omitempty"`  // 错误子代码描述
-	TradeStatus string `json:"trade_status" bson:"trade_status"`
+	TradeStatus string `json:"trade_status" bson:"trade_status"`			//
 	TradeNo     string `json:"trade_no" bson:"trade_no"`                     //渠道訂單號
 }
 type AlpExceptionResponse struct {
