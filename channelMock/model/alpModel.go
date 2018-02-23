07a3fa1eb7b9aa0bb50f68f96258b5b589ec467c
mating0815 <sunny.ma@cardinfolink.com>
2018-02-23 15:25:32 +0800

    add channelorderNum and fix bugs

--- channelMock/model/alpModel.go
+++ channelMock/model/alpModel.go
@@ -11,4 +11,14 @@ type AlpCommonResponse struct {
 	SubCode     string `json:"sub_code,omitempty" bson:"sub_code,omitempty"` // 错误子代码
 	SubMsg      string `json:"sub_msg,omitempty"  bson:"sub_msg,omitempty"`  // 错误子代码描述
 	TradeStatus string `json:"trade_status" bson:"trade_status"`
+	TradeNo     string `json:"trade_no" bson:"trade_no"`                     //渠道訂單號
+}
+type AlpExceptionResponse struct {
+	Sign        string `json:"sign" bson:"sign"`                             // 签名
+	Code        string `json:"code" bson:"code"`                             // 结果码
+	Msg         string `json:"msg" bson:"msg"`                               // 结果码描述
+	SubCode     string `json:"sub_code,omitempty" bson:"sub_code,omitempty"` // 错误子代码
+	SubMsg      string `json:"sub_msg,omitempty"  bson:"sub_msg,omitempty"`  // 错误子代码描述
+	TradeStatus string `json:"trade_status" bson:"trade_status"`
+	TradeNo     string `json:"trade_no" bson:"trade_no"`                     //渠道訂單號
 }
